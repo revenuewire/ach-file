1c747ac636d6cda9cc5cb6f4ea40f6f7a51601c7
Zullybur <mattcasiro@gmail.com>
2018-05-30 11:58:07 -0700

    Create File Header component

---
+++ src/ACH/ValidationException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mcasiro
+ * Date: 2018-05-28
+ * Time: 16:49
+ */
+
+namespace RW\ACH;
+
+
+class ValidationException extends \Exception {}
---
+++ tests/ACH/PaymentFileTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mcasiro
+ * Date: 2018-05-28
+ * Time: 13:32
+ */
+
+namespace RW\Tests\ACH;
+
+
+use PHPUnit\Framework\TestCase;
+use RW\ACH\PaymentFile;
+
+class PaymentFileTest extends TestCase
+{
+    public function testStopWarnings()
+    {
+        $this->assertTrue(true);
+    }
+}
